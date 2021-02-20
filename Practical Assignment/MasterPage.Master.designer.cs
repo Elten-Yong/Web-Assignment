e11ad72d043b542e8d517c00f02b30417d7ed3e0
TzeKin Ng <tzekinn123@gmail.com>
2021-02-21 00:06:55 +0800

    using menu for nav bar by NTK

--- Practical Assignment/MasterPage.Master.designer.cs
+++ Practical Assignment/MasterPage.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Practical_Assignment
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// SiteMapDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
-
-        /// <summary>
         /// TopMenuBar control.
         /// </summary>
         /// <remarks>
