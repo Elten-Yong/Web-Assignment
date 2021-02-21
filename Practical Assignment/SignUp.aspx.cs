04a2fffd04c4a216d16f427ff513eb9e519ec225
teejoohong0823 <teejh-wp18@student.tarc.edu.my>
2021-02-22 02:47:29 +0800

    tjh

--- Practical Assignment/SignUp.aspx.cs
+++ Practical Assignment/SignUp.aspx.cs
@@ -11,6 +11,9 @@ namespace Practical_Assignment
 {
     public partial class SignUp : System.Web.UI.Page
     {
+        Char accountType;
+        Boolean duplicate = false;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             SignUpMultiView.ActiveViewIndex = 0;
@@ -18,11 +21,13 @@ namespace Practical_Assignment
 
         protected void btnArtist_Click(object sender, EventArgs e)
         {
+            accountType = 'a';
             changeView();
         }
 
         protected void btnCustomer_Click(object sender, EventArgs e)
         {
+            accountType = 'c';
             changeView();
         }
         protected void changeView()
@@ -32,14 +37,81 @@ namespace Practical_Assignment
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
+            //create connection
             SqlConnection con;
             string strcon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
             con = new SqlConnection(strcon);
             con.Open();
 
+            if(accountType == 'a')
+            {
+                //retrieve data
+                string strSelect = "SELECT ArtistID, Username FROM [Artist]";
+                //specify what is the command , what is the connection string
+                SqlCommand cmdSelect = new SqlCommand(strSelect, con);
 
+                SqlDataReader dtr = cmdSelect.ExecuteReader();
+
+                
+                if (dtr.HasRows)
+                {
+                    while (dtr.Read())
+                    {
+                        if (txtUsername.Text.Equals(dtr["Username"]))
+                        {
+                            duplicate = true;
+                            lblDuplicate.Text = "Username existed, please use another username.";
+                            SignUpMultiView.ActiveViewIndex = 1; //stay in current page
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //retrieve data
+                string strSelect = "SELECT CustomerID, Username FROM [Customer]";
+                //specify what is the command , what is the connection string
+                SqlCommand cmdSelect = new SqlCommand(strSelect, con);
+
+                SqlDataReader dtr = cmdSelect.ExecuteReader();
+
+                if (dtr.HasRows)
+                {
+                    while (dtr.Read())
+                    {
+                        if (txtUsername.Text.Equals(dtr["Username"]))
+                        {
+                            duplicate = true;
+                            lblDuplicate.Text = "Username existed, please use another username.";
+                            SignUpMultiView.ActiveViewIndex = 1; //stay in current page
+                        }
+                    }
+                }
+            }
 
             con.Close();
+
+            con.Open();
+            if (!duplicate) //no repeated username
+            {
+                if(accountType == 'a')
+                {
+
+                    string strInsert = "INSERT INTO [Artist] (ArtistID, Username, Password, Email, SecurityQuestion, SecurityAnswer) VALUES (@ArtistID, @Username, @Password, @Email,@SecurityQuestion,@SecurityAnswer)";
+
+                    SqlCommand cmdInsert = new SqlCommand(strInsert, con);
+
+                    
+
+                }
+
+
+
+           
+            }
+            con.Close();
+
+          
         }
 
         protected void btnCancel_Click(object sender, EventArgs e)
