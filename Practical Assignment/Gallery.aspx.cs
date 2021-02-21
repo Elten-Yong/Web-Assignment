675217f45415b066550d50de812e946d5fca150a
TzeKin Ng <tzekinn123@gmail.com>
2021-02-22 01:29:08 +0800

    by ntk

--- Practical Assignment/Gallery.aspx.cs
+++ Practical Assignment/Gallery.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
 
 namespace Practical_Assignment
 {
@@ -12,7 +16,55 @@ namespace Practical_Assignment
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
+            SqlConnection con;
+            string strcon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            con = new SqlConnection(strcon);
+            con.Open();
 
+            string strSelect = "Select * from Gallery where DrawID=@DrawID";
+
+            SqlCommand cmdSelect = new SqlCommand(strSelect, con);
+
+            cmdSelect.Parameters.AddWithValue("@DrawID", "DR001");
+
+            SqlDataReader dtrGallery = cmdSelect.ExecuteReader();
+
+            //String test = dtrGallery["Image"].ToString();
+            //byte[] testByte = Encoding.ASCII.GetBytes(test);
+
+            //byte[] imgBytes = (byte[])dtrGallery["Image"];
+            //string strBase64 = Convert.ToBase64String(imgBytes);
+
+            if (dtrGallery.HasRows)
+            {
+                while (dtrGallery.Read())
+                {
+                    //msg = "Draw Name = " + dtrGallery["Name"].ToString();
+                    //Image1.Image = "data:image/png;base64," + strBase64;
+                }
+            }
+
+            con.Close();
+
+            /*
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("spGetImageById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter paramId = new SqlParameter()
+            {
+                ParameterName = "@Id",
+                Value = Request.QueryString["Id"]
+            };
+            cmd.Parameters.Add(paramId);
+
+            byte[] bytes = (byte[])cmd.ExecuteScalar();
+            string strBase64 = Convert.ToBase64String(bytes);
+
+            Image1.ImageUrl = "data:Image/png;base64," + strBase64;
+            */
+        }
+        
     }
 }
