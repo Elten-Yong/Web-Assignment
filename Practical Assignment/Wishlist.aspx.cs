c05ce04c7d4805efa74ac7be2a23eb6ada8db2c3
Jason Tang <JasonGod2000@hotmail.com>
2021-02-22 15:50:31 +0800

    by tsw

---
+++ Practical Assignment/Wishlist.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Practical_Assignment
+{
+    public partial class Wishlist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadImages();
+        }
+        private void LoadImages()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select Price, Image from Gallery", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+
+                con.Close();
+            }
+        }
+
+    }
+}
