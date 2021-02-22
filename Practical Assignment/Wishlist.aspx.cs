6d36c44e69bba92262212d4ecd8f920e0fbed0ff
Yong-Wei-Han <yongwh-wp18@student.tarc.edu.my>
2021-02-22 15:59:22 +0800

    commit by ywh

--- Practical Assignment/Wishlist.aspx.cs
+++ Practical Assignment/Wishlist.aspx.cs
@@ -8,28 +8,16 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
 
+
 namespace Practical_Assignment
 {
     public partial class Wishlist : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            LoadImages();
-        }
-        private void LoadImages()
-        {
-            string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(cs))
-            {
-                SqlCommand cmd = new SqlCommand("Select Price, Image from Gallery", con);
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                GridView1.DataSource = rdr;
-                GridView1.DataBind();
-
-                con.Close();
-            }
+            
         }
 
+        
     }
 }
