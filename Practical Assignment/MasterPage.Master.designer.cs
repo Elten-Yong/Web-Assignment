5d05573481fb877a2468716d4016a240a67811ce
samson <samso@DESKTOP-COMLFS9>
2021-02-23 18:28:20 +0800

    commit by jx

--- Practical Assignment/MasterPage.Master.designer.cs
+++ Practical Assignment/MasterPage.Master.designer.cs
@@ -78,15 +78,6 @@ namespace Practical_Assignment
         protected global::System.Web.UI.WebControls.Label ArtistUsername;
 
         /// <summary>
-        /// LinkButton2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton2;
-
-        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
