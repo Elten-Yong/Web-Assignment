f7ceb3264c9c61d9f6863091a7e210573b8a2a7c
Jason Tang <JasonGod2000@hotmail.com>
2021-02-24 00:09:26 +0800

    tsw

--- Practical Assignment/confirmOrder.aspx.cs
+++ Practical Assignment/confirmOrder.aspx.cs
@@ -54,7 +54,7 @@ namespace Practical_Assignment
                 cmdInsert.Parameters.AddWithValue("@OrderID", orderID);
                 cmdInsert.Parameters.AddWithValue("@CustomerID", Session["Value"]);
                 cmdInsert.Parameters.AddWithValue("@DrawID", e.CommandArgument.ToString());
-                cmdInsert.Parameters.AddWithValue("@Date", DateTime.Now.ToString());
+                cmdInsert.Parameters.AddWithValue("@Date", DateTime.Now);
 
                 int numRowAffected = cmdInsert.ExecuteNonQuery();
 
@@ -70,11 +70,69 @@ namespace Practical_Assignment
                     ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Bought Failed! " + "');", true);
                 }
                 con.Close();
+
+                SqlConnection con1;
+                string strcon1 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                con1 = new SqlConnection(strcon1);
+                string addedDraw = e.CommandArgument.ToString();
+                Boolean duplicate = false;
+                //checking value
+                con1.Open();
+                string strSelectChecking1 = "Select * from CartGallery Where DrawID=@DrawID and CustomerID = @CustomerID";
+                SqlCommand cmdSelect1 = new SqlCommand(strSelectChecking1, con);
+
+                cmdSelect1.Parameters.AddWithValue("@CustomerID", Session["Value"]);
+                cmdSelect1.Parameters.AddWithValue("@DrawID", addedDraw);
+
+                SqlDataReader dtr1 = cmdSelect1.ExecuteReader();
+
+                if (dtr1.HasRows)
+                {
+                    while (dtr1.Read())
+                    {
+                        if (addedDraw.Equals(dtr1["DrawID"]) && Session["Value"].Equals(dtr1["CustomerID"]))
+                        {
+                            duplicate = true;
+                            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Already added! " + "');", true);
+                        }
+                    }
+                }
+
+                con1.Close();
+
+                //insert value
+                if (!duplicate)
+                {
+                    con.Open();
+
+                    string strInsert1 = "Insert into CartGallery (CustomerID, DrawID, Name, Price, Image) Values (@CustomerID, @DrawID, @Name, @Price, @Image)";
+
+                    SqlCommand cmdInsert1 = new SqlCommand(strInsert1, con1);
+                    cmdInsert.Parameters.AddWithValue("@CustomerID", Session["Value"]);
+                    cmdInsert.Parameters.AddWithValue("@DrawID", addedDraw);
+                    cmdInsert.Parameters.AddWithValue("@Name", "Name");
+                    cmdInsert.Parameters.AddWithValue("@Price", "Price");
+                    cmdInsert.Parameters.AddWithValue("@Image", "Image");
+
+                    int numRowAffected1 = cmdInsert1.ExecuteNonQuery();
+                    if (numRowAffected1 > 0)
+                    {
+                        // return insert success
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Added successfully! " + "');", true);
+                    }
+                    else
+                    {
+                        // return insert failed
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "update failed! " + "');", true);
+                    }
+                    con.Close();
+                }
             }
             else
             {
                 Response.Redirect("Gallery.aspx");
             }
+
         }
     }
 }
